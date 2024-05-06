ae7175fabaaf594d02204a4db9610b175ade73b2
Carolina Alonso <carolina.alonso.depablos@iesjulianmarias.es>
2024-05-06 21:43:21 +0200

    V3.0

--- TareaGIT_codigo.java
+++ TareaGIT_codigo.java
@@ -7,8 +7,8 @@ public class TareaGIT_codigo {
     public static void main(String[] args) {
 
         /*ENUNCIADO
-            Pida 11 números por teclado y saque lo siguiente por pantalla: 
-                - Suma de esos 11 números.
+            Pida 20 números por teclado y saque lo siguiente por pantalla: 
+                - Suma de esos 20 números.
                 - Cuántos números positivos hay.
                 - Cuántos números negativos hay. 
          */
@@ -17,7 +17,7 @@ public class TareaGIT_codigo {
 
         // Pedir 11 números por teclado
         double[] numeros = new double[10];
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.print("Introduce el número " + (i + 1) + ": ");
             numeros[i] = scanner.nextDouble();
         }
